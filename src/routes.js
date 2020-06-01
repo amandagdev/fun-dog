e56538135e3c9da12793f09b8525a27bc73f437f
amandagdev <tiamandagomes@gmail.com>
2020-06-01 03:02:27 -0300

    hashrouter

--- src/routes.js
+++ src/routes.js
@@ -1,15 +1,17 @@
 import React from 'react';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, HashRouter } from 'react-router-dom';
 import Home from './pages/home/';
 import Dogs from './pages/dogs';
 
 export default function RoutesApp() {
   return (
     <>
-      <Routes>
-        <Route path="/" element={<Home />}></Route>
-        <Route path="/dogs" element={<Dogs />}></Route>
-      </Routes>
+      <HashRouter>
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/dogs" element={<Dogs />}></Route>
+        </Routes>
+      </HashRouter>
     </>
   );
 }
