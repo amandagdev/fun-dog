a941617e6fdb9919f88001384fe438ec280e5e01
amandagdev <tiamandagomes@gmail.com>
2020-06-01 01:51:39 -0300

    Updates

---
+++ precache-manifest.14c48cf6a0480d25290d7a2364ed723a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "41a845e49ac9fcf31c1cd00a8a43ad7c",
+    "url": "/fun-dog/index.html"
+  },
+  {
+    "revision": "c894de7d7d176c614fb4",
+    "url": "/fun-dog/static/css/main.45fa616e.chunk.css"
+  },
+  {
+    "revision": "de4b88c55dcb262bfdbe",
+    "url": "/fun-dog/static/js/2.7b4f5bc1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/fun-dog/static/js/2.7b4f5bc1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c894de7d7d176c614fb4",
+    "url": "/fun-dog/static/js/main.4b6d56cc.chunk.js"
+  },
+  {
+    "revision": "ba0f183828c7571eab4e",
+    "url": "/fun-dog/static/js/runtime-main.cf291f19.js"
+  },
+  {
+    "revision": "f37ed8f3692a61b17c414d4dfeb91a27",
+    "url": "/fun-dog/static/media/logo.f37ed8f3.PNG"
+  }
+]);
